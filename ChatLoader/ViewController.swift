d490ccad491cc437c48efa8fe857bae345ce4c7d
Paul Whiten <chatshots.app@gmail.com>
2021-02-11 23:05:35 +0800

    Initial Commit

---
+++ ChatLoader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatLoader
+//
+//  Created by Paul Michael Whiten on 11/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
